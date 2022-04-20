7a7b1010bb39f12ba7139681b9137b0d966083a6
Megha Sharma <mns2162@columbia.edu>
2022-04-20 01:52:31 -0400

    Timer for steak cooking

---
+++ IncrEDIBLES/Assets/Scripts/fryingFood.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fryingFood : MonoBehaviour
+{
+    public GameObject cookTimer;
+    [SerializeField] public Image greenFill;
+    public GameObject overcookTimer;
+    [SerializeField] public Image redFill;
+    public int Duration = 2000;
+    private int remainingDuration;
+    private Vector3 position;
+    private Quaternion rotation;
+    private GameObject collisionObject;
+    private GameObject newPrefab;
+    public GameObject steakPrefab;
+    public GameObject steakOvercookedPrefab;
+
+    void Start()
+    {
+        cookTimer.SetActive(false);
+        overcookTimer.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Ingredient put in frying pan");
+        collisionObject = collision.gameObject;
+        position = collisionObject.transform.position;
+        rotation = collisionObject.transform.rotation;
+
+        if (collision.gameObject.CompareTag("Steak"))
+        {
+            Debug.Log("Steak put in frying pan");
+            cookTimer.SetActive(true);
+            newPrefab = steakPrefab;
+            Begin(Duration, greenFill);
+        }
+
+    }
+
+    private void Begin(int Second, Image uiFill)
+    {
+        remainingDuration = Second;
+        StartCoroutine(UpdateTimer(uiFill));
+    }
+
+    private IEnumerator UpdateTimer(Image uiFill)
+    {
+        while(remainingDuration>0)
+        {
+            uiFill.fillAmount = Mathf.InverseLerp(0, Duration, remainingDuration);
+            remainingDuration--;
+            Debug.Log("timer -1");
+            yield return new WaitForSeconds(0.2f);
+        }
+        OnEnd();
+        if(cookTimer.activeSelf)
+        {
+            cookTimer.SetActive(false);
+            overcookTimer.SetActive(true);
+            newPrefab = steakOvercookedPrefab;
+            Begin(Duration, redFill);
+        }
+        else 
+        {
+            overcookTimer.SetActive(false);
+        }
+        
+    }
+
+    private void OnEnd()
+    {
+        Destroy(collisionObject);
+        collisionObject = Instantiate(newPrefab, position, rotation);
+    }
+}
