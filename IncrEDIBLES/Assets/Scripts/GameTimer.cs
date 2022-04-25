62b362e9e6ff7ffea0521649dcfdd379fe536240
monicalin22 <45611693+monicalin22@users.noreply.github.com>
2022-04-25 15:26:48 -0400

    Added a timer for the game.
    Counts down from 3 minutes the second the game starts. (Scene switches from start.)

---
+++ IncrEDIBLES/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
+
+public class GameTimer : MonoBehaviour
+{
+    public float timeValue = 180;
+    public Text timerText;
+    // public string endGame;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+            // End the game
+            // SceneManager.LoadScene(endGame);
+        }
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
