e4ca02e0029910a7f54e5972daea5d0b34127674
eureyuri <eureynoguchi@gmail.com>
2022-04-25 23:59:27 -0400

    feat: generate recipe cards

--- IncrEDIBLES/Assets/Scripts/Recipes.cs
+++ IncrEDIBLES/Assets/Scripts/Recipes.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = System.Random;
 using System;
 using System.Linq;
@@ -9,19 +10,24 @@ public class Recipes : MonoBehaviour
 {
     public TextAsset jsonRecipes;
     public AllRecipe recipes;
+    public GameObject recipeCard2;
+    public GameObject recipeCard3;
+    public GameObject recipeCard4;
+    public Sprite sampleDish;
+    public Sprite pasta;
+    public Sprite fish;
+    public Sprite mushroom;
+    public Sprite tomato;
+    public Sprite cheese;
+    public Sprite meat;
+    public Sprite cut;
+    public Sprite boil;
+    public Sprite saute;
+
     public static RecipeInfo[] currRecipes;
     private readonly int NUM_RECIPES = 5;
     private int allRecipeCount;
     Random rand = new Random();
-
-    // [System.Serializable]
-    // public class Ingredient {
-    //     public string food;
-
-    //     public override string ToString() {
-    //         return food;
-    //     }
-    // }
 
     [System.Serializable]
     public class RecipeInfo {
@@ -32,13 +38,14 @@ public class Recipes : MonoBehaviour
 
     [System.Serializable]
     public class Directions {
-        public string[] directions;
+        public string name;
+        public string[] procedure;
     }
 
     [System.Serializable]
     public class AllRecipe {
         public RecipeInfo[] recipes;
-        public Directions directions;
+        public Directions[] directions;
     }
 
     void Start()
@@ -48,8 +55,7 @@ public class Recipes : MonoBehaviour
 
         allRecipeCount = recipes.recipes.Length;
         GenerateRecipes();
-
-        Debug.Log("currRecipes: " + currRecipes);
+        InstantiateRecipes();
     }
 
     private void GenerateRecipes() {
@@ -59,6 +65,84 @@ public class Recipes : MonoBehaviour
     public RecipeInfo PickRecipe() {
         int i = rand.Next(allRecipeCount);
         return recipes.recipes[i];
+    }
+
+    private void InstantiateRecipes() {
+        for (int i = 0; i < NUM_RECIPES; i++) InstantiateRecipe(currRecipes[i], i);
+    }
+
+    public void InstantiateRecipe(RecipeInfo recipe, int index) {
+        GameObject recipeCard;
+        int ingredientsLen = recipe.ingredients.Length;
+        switch (ingredientsLen) {
+            case 2: recipeCard = recipeCard2; break;
+            case 3: recipeCard = recipeCard3; break;
+            case 4: recipeCard = recipeCard4; break;
+            default: recipeCard = null; break;
+        }
+
+        if (recipeCard == null) return;
+
+        float xPos;
+        switch (index) {
+            case 0: xPos = -19f; break;
+            case 1: xPos = -7.99f; break;
+            case 2: xPos = 3.1f; break;
+            case 3: xPos = 14.16f; break;
+            case 4: xPos = 25.5f; break;
+            default: xPos = 0; break;
+        }
+
+        recipeCard.transform.GetChild(0).GetComponent<Image>().sprite = sampleDish;
+
+        Transform ingredientList = recipeCard.transform.GetChild(1);
+        Transform ingredientInst1 = recipeCard.transform.GetChild(2);
+        Transform ingredientInst2 = recipeCard.transform.GetChild(3);
+        Sprite ingredientToAdd;
+        for (int i = 0; i < ingredientsLen; i++) {
+            switch (recipe.ingredients[i]) {
+                case "pasta": ingredientToAdd = pasta; break;
+                case "fish": ingredientToAdd = fish; break;
+                case "mushroom": ingredientToAdd = mushroom; break;
+                case "tomato": ingredientToAdd = tomato; break;
+                case "cheese": ingredientToAdd = cheese; break;
+                case "meat": ingredientToAdd = meat; break;
+                default: ingredientToAdd = null; break;
+            }
+            ingredientList.GetChild(i).GetComponent<Image>().sprite = ingredientToAdd;
+
+            string[] procedure = null;
+            Debug.Log("searching procedure 1: " + recipe.ingredients[i]);
+            for (int j = 0; j < recipes.directions.Length; j++) {
+                Debug.Log("searching procedure 2: " + recipes.directions[j].name);
+                if (recipe.ingredients[i] == recipes.directions[j].name) {
+                    procedure = recipes.directions[j].procedure;
+                    break;
+                }
+            }
+            Debug.Log("final procedure: " + procedure);
+            for (int j = 0; j < procedure.Length; j++) {
+                Sprite instToAdd;
+                switch (procedure[j]) {
+                    case "boil": instToAdd = boil; break;
+                    case "cut": instToAdd = cut; break;
+                    case "saute": instToAdd = saute; break;
+                    default: instToAdd = null; break;
+                }
+
+                Debug.Log("appending procedure: " + procedure[j]);
+                if (j == 0) {
+                    ingredientInst1.GetChild(i).GetComponent<Image>().sprite = instToAdd;
+                } else if (j == 1) {
+                    ingredientInst2.GetChild(i).GetComponent<Image>().sprite = instToAdd;
+                }
+
+            }
+            Debug.Log("finished: " + i);
+        }
+
+        GameObject card = Instantiate(recipeCard, new Vector3(xPos, 25.1f, 0), Quaternion.identity) as GameObject;
+        card.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
     }
 
     public bool CheckComplete(string[] ingredients) {
