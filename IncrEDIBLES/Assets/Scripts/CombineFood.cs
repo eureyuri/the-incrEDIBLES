c260b32190e91f9015ea0bee4959ef8a203b31fa
eureyuri <eureynoguchi@gmail.com>
2022-04-27 21:30:23 -0400

    feat: cheese pasta

--- IncrEDIBLES/Assets/Scripts/CombineFood.cs
+++ IncrEDIBLES/Assets/Scripts/CombineFood.cs
@@ -7,6 +7,8 @@ public class CombineFood : MonoBehaviour
 {
     private List<string> foodOnPlate;
     public GameObject tomatoPasta;
+    public GameObject cheesePasta;
+    public GameObject intermediateDish;
 
     void Start() {
         foodOnPlate = new List<string>();
@@ -23,34 +25,61 @@ public class CombineFood : MonoBehaviour
     }
 
     private void AddOne(GameObject collided) {
-        collided.GetComponent<XRGrabInteractable>().enabled = false;
-        collided.GetComponent<Rigidbody>().isKinematic = true;
-        collided.transform.parent = this.transform;
-        collided.transform.localPosition = new Vector3(0, 0.1f, 0);
-        AddToPlate(collided);
+        if (IsValidFood(collided)) {
+            collided.GetComponent<XRGrabInteractable>().enabled = false;
+            collided.GetComponent<Rigidbody>().isKinematic = true;
+            collided.transform.parent = this.transform;
+            collided.transform.localPosition = new Vector3(0, 0.1f, 0);
+            AddToPlate(collided);
+        }
+    }
+
+    private bool IsValidFood(GameObject collided) {
+        return (collided.CompareTag("ReadyPasta") || collided.CompareTag("ReadyTomato") || collided.CompareTag("ReadyCheese"));
     }
 
     private void AddToPlate(GameObject collided) {
-        if (collided.CompareTag("ReadyPasta")) {
-            Debug.Log("CombineFood: add pasta");
-            if (!foodOnPlate.Contains("pasta")) foodOnPlate.Add("pasta");
-        } else if (collided.CompareTag("ReadyTomato")) {
-            Debug.Log("CombineFood: add tomato");
-            if (!foodOnPlate.Contains("tomato")) foodOnPlate.Add("tomato");
+        if (collided.CompareTag("ReadyPasta") && !foodOnPlate.Contains("pasta")) {
+            Debug.Log("CombineFood: added pasta");
+            foodOnPlate.Add("pasta");
+        } else if (collided.CompareTag("ReadyTomato") && !foodOnPlate.Contains("tomato")) {
+            foodOnPlate.Add("tomato");
+        } else if (collided.CompareTag("ReadyFish") && !foodOnPlate.Contains("fish")) {
+            foodOnPlate.Add("fish");
+        } else if (collided.CompareTag("ReadyMushroom") && !foodOnPlate.Contains("mushroom")) {
+            foodOnPlate.Add("mushroom");
+        } else if (collided.CompareTag("ReadyCheese") && !foodOnPlate.Contains("cheese")) {
+            Debug.Log("CombineFood: added cheese");
+            foodOnPlate.Add("cheese");
+        } else if (collided.CompareTag("ReadyMeat") && !foodOnPlate.Contains("meat")) {
+            foodOnPlate.Add("meat");
         }
     }
 
     private void CombineTwo(GameObject collided) {
-        Debug.Log("CombineFood: AddTwo");
-        AddToPlate(collided);
+        if (IsValidFood(collided)) {
+            Debug.Log("CombineFood: AddTwo");
+            AddToPlate(collided);
 
-        Debug.Log("CombineFood: contains?: " + (foodOnPlate.Contains("pasta") && foodOnPlate.Contains("tomato")));
-        if (foodOnPlate.Contains("pasta") && foodOnPlate.Contains("tomato")) {
-            Destroy(transform.GetChild(0).gameObject);
-            Destroy(collided);
-            GameObject childObject = Instantiate(tomatoPasta) as GameObject;
-            childObject.transform.parent = transform;
-            childObject.transform.localPosition = new Vector3(0, 0.1f, 0);
+            if (foodOnPlate.Contains("pasta") && foodOnPlate.Contains("tomato")) {
+                Destroy(transform.GetChild(0).gameObject);
+                Destroy(collided);
+                GameObject childObject = Instantiate(tomatoPasta) as GameObject;
+                childObject.transform.parent = transform;
+                childObject.transform.localPosition = new Vector3(0, 0.1f, 0);
+            } else if (foodOnPlate.Contains("pasta") && foodOnPlate.Contains("cheese")) {
+                Destroy(transform.GetChild(0).gameObject);
+                Destroy(collided);
+                // FIXME: not correctly set as child of plate
+                GameObject childObject = Instantiate(cheesePasta) as GameObject;
+                childObject.transform.parent = transform;
+                childObject.transform.localPosition = new Vector3(0, 0.1f, 0);
+            }
+            // else {
+            //     GameObject childObject = Instantiate(intermediateDish) as GameObject;
+            //     childObject.transform.parent = transform;
+            //     childObject.transform.localPosition = new Vector3(0, 0.1f, 0);
+            // }
         }
     }
 }
