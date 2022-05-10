1ab99a056ecb3d600eb0c1787ba7ad91630e35ed
sl4684 <sl4684@columbia.edu>
2022-05-09 22:52:15 -0400

    fix number of recipe cards

--- IncrEDIBLES/Assets/Scripts/ScoreDisplay.cs
+++ IncrEDIBLES/Assets/Scripts/ScoreDisplay.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.XR;
 using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour
 {
     public Text scoreField;
+
+    private Vector3 relativePos;
+    public GameObject cameraOffset;
+    private Vector3 fakeForward;
+    private float distanceFactor = 0.761f;
     // Start is called before the first frame update
     void Start()
     {
-        scoreField.text = Score.score.ToString(); 
+        scoreField.text = Score.score.ToString();
+
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+        relativePos = cameraOffset.transform.position;
+        if(transform.localEulerAngles.y<=45 || transform.localEulerAngles.y>315)
+        {
+            relativePos += new Vector3(0.0f,0.0f,distanceFactor);
+        }
+        else if(transform.localEulerAngles.y<=135)
+        {
+            relativePos += new Vector3(distanceFactor,0.0f,0.0f);
+        }
+        else if(transform.localEulerAngles.y<=225)
+        {
+            relativePos += new Vector3(0.0f,0.0f,-distanceFactor);
+        }
+        else 
+        {
+            relativePos += new Vector3(-distanceFactor,0.0f,0.0f);
+        }
+        fakeForward = Camera.main.transform.forward;
+        fakeForward.y = 0.0f; 
+        fakeForward.Normalize(); 
+        transform.position = Vector3.Lerp(transform.position, relativePos + fakeForward, Time.deltaTime+1.0f);  
     }
 }
